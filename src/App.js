7c90a9e978f9cc4b526ae0ddd2bea02ed4d152c1
AJINKYA <ajinkya.wankhade@knolskape.com>
2020-04-26 20:09:04 +0530

    Add basic error boundary component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import UserListContainer from './containers/UserListContainer.js';
 import UserAddressesContainer from './containers/UserAddressesContainer.js';
 import AppHeader from './components/appHeader';
+import ErrorBoundary from './ErrorBoundary';
 
 const App = () => {
 
@@ -13,28 +14,30 @@ const App = () => {
   }
 
   return (
-    <div className="App">
-      <AppHeader />
-      <div className="content">
-        <UserListContainer
-          selectedUser={selectedUser}
-          setSelectedUser={setUser}
-        />
-        {
-          selectedUser ?
-            <UserAddressesContainer
-              key={`user_${selectedUser.id}`}
-              selectedUser={selectedUser}
-            />
-            :
-            <div style={{ width: '100%' }}>
-              <h2 style={{ color: '#666', textAlign: 'center' }}>
-                No user selected
+    <ErrorBoundary >
+      <div className="App">
+        <AppHeader />
+        <div className="content">
+          <UserListContainer
+            selectedUser={selectedUser}
+            setSelectedUser={setUser}
+          />
+          {
+            selectedUser ?
+              <UserAddressesContainer
+                key={`user_${selectedUser.id}`}
+                selectedUser={selectedUser}
+              />
+              :
+              <div style={{ width: '100%' }}>
+                <h2 style={{ color: '#666', textAlign: 'center' }}>
+                  No user selected
 				      </h2>
-            </div>
-        }
+              </div>
+          }
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 }
 
